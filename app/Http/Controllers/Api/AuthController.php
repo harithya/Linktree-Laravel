6d9a423e447116739c83dce80c4cd46e393f38e1
Harithya Wisesa <harithya77@gmail.com>
2023-04-17 19:08:00 +0700

    menambahkan login dan register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -11,7 +13,36 @@ class AuthController extends Controller
     {
         $request->validate([
             'email' => 'required|email',
-            'password' => 'required'
+            'password' => 'required|string'
         ]);
+
+        $auth = Auth::attempt($request->all());
+        if ($auth) {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response(['user' => $user, 'token' => $token]);
+        } else {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+            'name' => 'required|string'
+        ]);
+
+        $user = User::create(array_merge(
+            $request->except('password'),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        if ($user) {
+            return response(['user' => $user, 'message' => 'User created successfully'], 201);
+        } else {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, "login"]);
+    Route::post('register', [AuthController::class, "register"]);
 });
