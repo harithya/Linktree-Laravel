56a1c821fe0b1a0cff34f51be35d1907922a3046
Harithya Wisesa <harithya77@gmail.com>
2023-04-20 14:13:51 +0700

    menambahkan migration links

---
+++ database/migrations/2023_04_20_071007_create_links_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->string('url');
+            $table->string('image')->nullable();
+            $table->tinyInteger('is_active')->default(1);
+            $table->foreignUuid('users_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
